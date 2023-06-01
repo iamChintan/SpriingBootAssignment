05f0066f82a9b9d7a4dc5bd3e50fae6a4df4b2fc
iamChintan <88285492+iamChintan@users.noreply.github.com>
2023-06-01 19:38:43 -0400

    task4 done

---
+++ SpringBootAssignment/SpringBootAssignment/src/main/java/com/springboot/assignment/SpringBootAssignment/MyController.java
@@ -0,0 +1,13 @@
+package com.springboot.assignment.SpringBootAssignment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
---
+++ SpringBootAssignment/SpringBootAssignment/src/main/java/com/springboot/assignment/SpringBootAssignment/SpringBootAssignmentApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.assignment.SpringBootAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SpringBootAssignmentApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssignmentApplication.class, args);
+	}
+
+}
